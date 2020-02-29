cd1572a83aceb4be120810d17a5bbfaca2775dc3
Frodo <jvfaggion@hotmail.com>
2020-02-28 21:10:15 -0300

    Project Created

---
+++ src/main/java/io/steamreviewbot/SteamreviewbotApplication.java
@@ -0,0 +1,13 @@
+package io.steamreviewbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamreviewbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamreviewbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/steamreviewbot/SteamreviewbotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.steamreviewbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamreviewbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
