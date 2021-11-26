651264c535c5c6d376e3b483ff0a54da32a03ec8
João Faggion <joaovitor.faggion@gmail.com.br>
2021-11-26 15:13:09 -0300

    generate stats por data

--- src/main/java/io/steamreviewbot/resources/PageStatisticsResource.java
+++ src/main/java/io/steamreviewbot/resources/PageStatisticsResource.java
@@ -2,11 +2,15 @@ package io.steamreviewbot.resources;
 
 import io.steamreviewbot.services.PageStatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 @RequestMapping(value="/statistics")
@@ -15,10 +19,15 @@ public class PageStatisticsResource {
     @Autowired
     private PageStatisticsService service;
 
-    @RequestMapping(value = "/generate", method = RequestMethod.POST)
-    public String pulse() throws IOException {
+    @RequestMapping(value = "/generate/{date}", method = RequestMethod.POST)
+    public String pulse(@PathVariable String date) throws IOException {
 
-        service.generateStatsPost();
+        try {
+            Date data = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+            service.generateStatsPost(data);
+        } catch(ParseException ex){
+            return "data com formato errado, esperado: dd/MM/yyyy";
+        }
 
         return "done";
     }
